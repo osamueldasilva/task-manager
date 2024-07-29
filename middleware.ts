4f3debed47538d7d8f65fe10e2c8a671f84b5542
osamueldasilva <osamueldasilva1@gmail.com>
2024-07-29 19:13:00 -0300

    Migração do schema do prisma

--- middleware.ts
+++ middleware.ts
@@ -31,5 +31,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/task/:path*", "/login"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
