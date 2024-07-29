d93a4b1ef3c59c960c7d1dd7c543d78ad142b7ae
osamueldasilva <osamueldasilva1@gmail.com>
2024-07-29 19:00:58 -0300

    finalização da verificação do token

--- middleware.ts
+++ middleware.ts
@@ -7,30 +7,29 @@ export default async function middleware(req: NextRequest) {
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  if (!token) {
+  const { pathname } = req.nextUrl;
+
+  if (!token && pathname !== "/login") {
     const url = req.nextUrl.clone();
     url.pathname = "/login";
-
     return NextResponse.redirect(url);
   }
 
-  if (req.nextUrl.pathname === "/") {
-    if (!token) {
-      const url = req.nextUrl.clone();
-      url.pathname = "/login";
+  if (token && pathname === "/login") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/task";
+    return NextResponse.redirect(url);
+  }
 
-      return NextResponse.redirect(url);
-    } else {
-      const url = req.nextUrl.clone();
-      url.pathname = "/task";
-
-      return NextResponse.redirect(url);
-    }
+  if (pathname === "/") {
+    const url = req.nextUrl.clone();
+    url.pathname = token ? "/task" : "/login";
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/task/:path*"],
+  matcher: ["/", "/task/:path*", "/login"],
 };
