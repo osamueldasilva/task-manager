87468c4ddc68a26c595053658cc92531bf06413e
osamueldasilva <osamueldasilva1@gmail.com>
2024-07-29 19:29:17 -0300

    mudança no estilo do dark mode

--- components/dark-mode/dark-mode.tsx
+++ components/dark-mode/dark-mode.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MoonIcon, SunIcon } from "lucide-react";
+import { Moon, MoonIcon, Sun, SunIcon } from "lucide-react";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
@@ -18,17 +18,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="rounded-full">
-          <SunIcon
-            className={`h-[1.2rem] w-[1.2rem] transition-all ${
-              theme === "light" ? "rotate-0 scale-100" : "rotate-90 scale-0"
-            } dark:-rotate-90 dark:scale-0`}
-          />
-          <MoonIcon
-            className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
-              theme === "dark" ? "rotate-0 scale-100" : "rotate-90 scale-0"
-            } dark:rotate-0 dark:scale-100`}
-          />
+        <Button variant="outline" className="rounded-full" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
